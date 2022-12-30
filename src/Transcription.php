8307e5fbacc5f5507aec448c351d575190fc58c7
Jonathan Bell <jbell@benevity.com>
2022-12-30 15:44:55 -0800

    End of second episode

--- src/Transcription.php
+++ src/Transcription.php
@@ -5,18 +5,33 @@ namespace Jonathanbell\Firstphpcli;
 class Transcription
 {
 
-  protected string $file;
+  protected array $lines;
 
-  public function __toString(): string {
-    return $this->file;
-  }
-
-  public static function load(string $path):string
+  public static function load(string $path): self
   {
     $instance = new static();
-    $instance->file = file_get_contents($path);
+
+    $instance->lines = $instance->discardIrrelevantLines(file($path));
 
     return $instance;
+  }
+
+  public function lines(): array
+  {
+    return $this->lines;
+  }
+
+  protected function discardIrrelevantLines(array $lines): array {
+    $lines = array_map('trim', $lines);
+
+    return array_values(array_filter($lines, callback: function ($line) {
+      $line = trim($line);
+      return !str_contains($line, 'WEBVTT') && !empty($line) && !is_numeric($line);
+    }));
+  }
+
+  public function __toString(): string {
+    return implode('', $this->lines);
   }
 
 }
--- tests/TranscriptionTest.php
+++ tests/TranscriptionTest.php
@@ -8,11 +8,26 @@ use PHPUnit\Framework\TestCase;
 class TranscriptionTest extends TestCase
 {
 
-  public function testItLoadsAVttFile() {
-    $transcription = Transcription::load(__DIR__.'/stubs/basic-example.vtt');
-    $expected = file_get_contents(__DIR__.'/stubs/basic-example.vtt');
+  public function testItLoadsAVttFileAsAString() {
+    $file = __DIR__ . '/stubs/basic-example.vtt';
+    $transcription = Transcription::load($file);
 
-    $this->assertEquals($expected, $transcription);
+    $this->assertStringContainsString('Here is a', $transcription);
+    $this->assertStringContainsString('example of a VTT file', $transcription);
   }
 
+  public function testItCanConvertAStringToAnArray() {
+    $file = __DIR__ . '/stubs/basic-example.vtt';
+
+    $this->assertCount(4, Transcription::load($file)->lines());
+  }
+
+  public function testItDiscardsIrrelevantVttLines() {
+    $file = __DIR__ . '/stubs/basic-example.vtt';
+
+    $transcription = Transcription::load($file);
+
+    $this->assertStringNotContainsString('WEBVTT', $transcription);
+    $this->assertCount(4, $transcription->lines());
+  }
 }
